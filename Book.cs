4da27df0159a3a7ce30ba78c49160cf2397e9983
Dor Kislev <dor.kislev@gmail.com>
2025-10-08 00:38:22 +0300

    Fixed Borrow/Return time tracking and TimeSpan calculation

--- Book.cs
+++ Book.cs
@@ -1,11 +1,12 @@
 namespace LibrarySystem
 {
     using System;
-    public class Book
+    public class Book : IBorrowable
     {
         protected string title;
         protected string author;
         public bool isBorrowed;
+        private DateTime borrowTime;
         public Book(string title, string author)
         {
             this.title = title;
@@ -20,32 +21,50 @@ namespace LibrarySystem
                 if (!this.isBorrowed)
                 {
                     this.isBorrowed = true;
-                    Console.WriteLine($"{reader.name} have borrowed '{this.title}' by {this.author}.\n");
+                    borrowTime = DateTime.Now;
+                    Console.WriteLine($"{reader.name} have borrowed '{this.title}' by {this.author}.");
+                    Console.WriteLine($"Borrowing time: {borrowTime}\n");
                     return true;
                 }
                 else
                 {
-                    Console.WriteLine($"Sorry {reader.name}, '{this.title}' is already borrowed.\n");
+                    Console.WriteLine($"Sorry {reader.name}, '{this.title}' is already borrowed.");
                     return false;
                 }
             }
             else
             {
-                Console.WriteLine($"{reader.name}'s Error: The book '{this.title}' is not found in this library\n");
+                Console.WriteLine($"{reader.name}'s Error: The book '{this.title}' is not found in this library");
                 return false;
             }
         }
+
+
         // Method to return the book
         public void Return(Reader reader)
         {
             if (this.isBorrowed)
             {
                 this.isBorrowed = false;
-                Console.WriteLine($"{reader.name} have returned '{this.title}'.\n");
+
+                DateTime now = DateTime.Now;
+                TimeSpan timeSpan = now - borrowTime;
+
+                Console.WriteLine($"{reader.name} have returned '{this.title}'.");
+                Console.WriteLine($"Returning time: {now}");
+
+                if (timeSpan.TotalDays > 7)
+                {
+                    Console.WriteLine($"Total borrowed duration: {timeSpan.Days} days, {timeSpan.Hours} hours, {timeSpan.Minutes} minutes. You have exceeded the 7-day borrowing limit and you will receive a fine.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Total borrowed duration: {timeSpan.Days} days, {timeSpan.Hours} hours, {timeSpan.Minutes} minutes. Thank you for returning the book on time!\n");
+                }
             }
             else
             {
-                Console.WriteLine($"'Sorry {reader.name},{this.title}' was not borrowed.\n");
+                Console.WriteLine($"Sorry {reader.name}, '{this.title}' was not borrowed.");
             }
         }
 
@@ -55,5 +74,4 @@ namespace LibrarySystem
             return $"Title:{this.title}, Author:{this.author}";
         }
     }
-    // Reader class representing a library member
 }
